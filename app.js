8e924162370c042d19d8b81d613bd31362b0bf33
Brian Thornburg <briant3275@gmail.com>
2021-11-03 00:31:24 -0500

    fixing objects

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 
 
 const hour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
-// let cookiesAmount = Math.floor(randomInRange(23,65) * 6.3)
+let total = 0
 
+function assignCustomerPerHour(max,min) {
+    // generate random customer amount
+    let range = max - min + 1;
+    return Math.round(Math.random() * range) + min;
+}
 
 let seattle = {
     location: `Seattle`,
@@ -11,47 +16,79 @@ let seattle = {
     maxCustomer: 65,
     avgCookies:  6.3,
     cookiesSalesPerHour: [],
-    assignCustomerPerHour: function() {
-        // generate random customer amount
-        let range = maxCustomer - minCustomer + 1;
-        return Math.floor(Math.random() * range) + min;
-    },
+    total: [],
     assignCookiePerHour: function() {
-        // generate random customer amount
-        let range = maxCustomer - minCustomer + 1;
-        return Math.floor((Math.floor(Math.random() * range) + min) * 6.3);
+        // generate random cookies per hour
+        const cookiesSalesPerHour = Math.round(assignCustomerPerHour(this.maxCustomer,this.minCustomer) * this.avgCookies);
+        console.log(this.assignCookiePerHour);
+        return cookiesSalesPerHour;
     
+    },
+    pushTotal: function() {
+        //push total onto end of cookieSalesPerHour array
+        cookieSalesPerHour.push(total);
     }
 }
 
-function cookies(seattle) {
-    for (let i = 0; i < hour.length; i +=1) {
-    // let maxCustomer = 65;
+//testing push
+// let cookieSalesPerHourNew = [total];
+// seattle.cookieSalesPerHour.push(cookieSalesPerHourNew);
+
+// function cookies() {
+for (let i = 0; i < hour.length; i +=1) {
+    seattle.cookiesSalesPerHour[i] = seattle.assignCookiePerHour();
+    total = total + seattle.cookiesSalesPerHour[i];
+    // seattle.cookieTotal = seattle.cookiesSalesPerHour[i];
+    //let maxCustomer = 65;
     // let minCustomer = 23;
-    let range = seattle.maxCustomer - seattle.minCustomer + 1;
-    let cookiesAmount = Math.floor(Math.floor(Math.random() * range + seattle.minCustomer) * 6.3)
-    console.log(cookiesAmount)
-    }
+    // let cookiesAmount = Math.round(assignCustomerPerHour(this.maxCustomer,this.minCustomer) * this.avgCookies)
+    // console.log(seattle.cookieSalesPerHour)
+    // console.log(seattle.cookieTotal)
 }
-cookies(seattle)
+// }
+// cookies()
 
 // function sum(cookies) {
 //     sum cookies;
 // }
 
+function doCookies(seattle) {
+    let seattleContainer = document.getElementById(`seattle`)
 
-let seattleContainer = document.getElementById(`seattle`)
+    // function arrayRender() {
+    // }
+    //grab elem append to
+    //create ne elems for display
+    //assign content to new elem
+    //append elem to first thing we grabbed
 
-let articleElem = document.createElement('article');
+    const articleElem = document.createElement('article');
+    seattleContainer.appendChild(articleElem)
 
-seattleContainer.appendChild(articleElem)
+    let h2Elem = document.createElement('h2');
+    articleElem.appendChild(h2Elem);
+    h2Elem.textContent = seattle.location;
 
-let h2Elem = document.createElement('h2');
-articleElem.appendChild(h2Elem);
-h2Elem.textContent = seattle.location;
+    let listElem = document.createElement('ul')
+    seattleContainer.appendChild(listElem)
 
-let ulElem = document.createElement('ul')
+    for (let i = 0; i < seattle.cookiesSalesPerHour.length; i += 1) {
+        const cookiePerHourElem = document.createElement('li');
+        listElem.appendChild(cookiePerHourElem);
+        const cookiePerHourText = seattle.cookiesSalesPerHour[i];
+        cookiePerHourElem.textContent = cookiePerHourText;
+    }
 
-// cookiesAmount: Math.floor(randomInRange(23,65) * 6.3),
+    const shopTotalElem = document.createElement('li')
+    listElem.appendChild(shopTotalElem);
+    const shopTotalText = total;
+    shopTotalElem.textContent = shopTotalText
 
+    let cookiePer = seattle.cookieSalesPerHour
+    seattleContainer.appendChild(cookiePer)
 
+    // cookiesAmount: Math.floor(randomInRange(23,65) * 6.3),
+
+}
+
+doCookies(seattle);
